8fc790196367f3bf7d69eb31545910acba90eefd
abubakr193 <salimboy19941409@gmail.com>
2022-01-05 21:04:54 +0500

    Js first lesson

---
+++ js/main.js
@@ -0,0 +1,13 @@
+alert("Assalomu Alaykum Xush Kelibsiz Akaxon");
+
+var userName = prompt("Akaxon ismingizni yozvorin!");
+var userSurname = prompt("Akaxon Familiyangizni ham yozvoring!");
+var Age = prompt("Akaxon yoshingiz nechida?");
+var Work = prompt("Akaxon nima ishlar bilan bandsiz?");
+var isMarried = prompt("Akaxon to'y qachon endi?");
+var hobby = prompt("Akaxon nimalarga qiziqasiz? Hobbiyingiz bormi?");
+var sport = prompt("Akaxon qaysi sport turiga qiziqasiz?");
+
+// conlose.log("Ismingiz: " + userName + "/n" + "Familiyaniz: " + userSurname + "/n" + "Yoshingiz" + Age + "/n" + "Ishingiz: " + Work + "/n" + "Bo'ydoqligingiz: " + isMarried + "/n" + "Hobbiyingiz: " + hobby + "/n" + "Sport turiga qiziqishingiz: " + sport);
+
+alert("Akaxon rahmat sizga kelib turing shunaqa Hursand bo'lamiz :-)");
